f271406fec6616a60dc882d33dde8dd538a3f34b
farid <faridhavasi@yahoo.com>
2022-04-28 17:58:44 +0430

    fix

--- tafazol/tafazol.py
+++ tafazol/tafazol.py
@@ -1,7 +1,10 @@
 def game(n1: int, n2: int):
-    if n1 > n2:
-        return n1 - n2
+    if n1 and n2 in range(0, 10):
+        pass
     else:
-        return n2 - n1
+        if n1 > n2:
+            return n1 - n2
+        else:
+            return n2 - n1
     
 print(game(3,5))    # Output: 2
